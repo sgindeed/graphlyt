4f190eaaba88806feb0e4b614930b315b51b9c81
Supratim <109270340+sgindeed@users.noreply.github.com>
2026-04-19 20:54:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# Start Postgres in the background
+# 1. Start Postgres in the background
 docker-entrypoint.sh postgres &
 
-# Wait for Postgres to be ready
+# 2. Wait for Postgres to be ready
 echo "Waiting for Postgres to start..."
-until pg_isready -h localhost -p 5432; do
+until pg_isready -h localhost -p 5432 -U postgres; do
   sleep 2
 done
 
-# Start the FastAPI App
-echo "Starting FastAPI..."
+# 3. Enter the backend folder and start the app
+echo "Moving to backend folder and starting FastAPI..."
+cd backend
 uvicorn main:app --host 0.0.0.0 --port 8000
